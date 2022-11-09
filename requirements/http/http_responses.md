8771248bec1ca3533df1cbb7eb5d43735f0a6211
DavidCarrilho <luan.david.oliver@gmail.com>
2022-11-09 01:18:56 -0300

    test: ensure HttpAdapter calls post without body

--- requirements/http/http_responses.md
+++ requirements/http/http_responses.md
@@ -4,7 +4,7 @@
 
 1. ✅ Request com verbo http válid (post)
 2. ✅ Passar nos headres o content type json
-3. Chamar request com o body correto
+3. ✅ Chamar request com o body correto
 4. OK - 200 e resposta com o dados
 5. No content - 204 e resposta sem o dados
 
--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -20,10 +20,11 @@ class HttpAdapter {
       'content-type': 'application/json',
       'accept': 'application/json',
     };
+    var jsonBody = body != null ? jsonEncode(body) : null;
     await client.post(
       url,
       headers: headers,
-      body: jsonEncode(body),
+      body: jsonBody,
     );
   }
 }
@@ -52,6 +53,15 @@ void main() {
           'accept': 'application/json',
         },
         body: '{"any_key":"any_value"}',
+      ));
+    });
+
+    test('Should call post without body', () async {
+      // act
+      await sut.request(url: url, method: 'post');
+      verify(client.post(
+        any,
+        headers: anyNamed('headers'),
       ));
     });
   });
