4536abcfacf0d1e166a03a5d49be3b0d6b90b86c
DavidCarrilho <luan.david.oliver@gmail.com>
2022-11-15 16:35:26 -0300

    refactor: page initialization to a helper method

--- test/ui/pages/login_page_test.dart
+++ test/ui/pages/login_page_test.dart
@@ -3,25 +3,36 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:prodev/ui/pages/pages.dart';
 
 void main() {
-  testWidgets('Should load with correct ', (WidgetTester tester) async {
+  Future<void> loadPage(WidgetTester tester) async {
     final loginPage = MaterialApp(home: LoginPage());
     await tester.pumpWidget(loginPage);
+  }
 
-    final emailTextChildren = find.descendant(of: find.bySemanticsLabel('Email'), matching: find.byType(Text));
+  testWidgets('Should load with correct ', (WidgetTester tester) async {
+    await loadPage(tester);
+
+    final emailTextChildren =
+        find.descendant(of: find.bySemanticsLabel('Email'), matching: find.byType(Text));
     expect(
       emailTextChildren,
       findsOneWidget,
-      reason: 'when a TextFormField has only one text child, means it has no errors, since one of the childs is always the label text',
+      reason:
+          'when a TextFormField has only one text child, means it has no errors, since one of the childs is always the label text',
     );
 
-    final passwordTextChildren = find.descendant(of: find.bySemanticsLabel('Senha'), matching: find.byType(Text));
+    final passwordTextChildren =
+        find.descendant(of: find.bySemanticsLabel('Senha'), matching: find.byType(Text));
     expect(
       passwordTextChildren,
       findsOneWidget,
-      reason: 'when a TextFormField has only one text child, means it has no errors, since one of the childs is always the label text',
+      reason:
+          'when a TextFormField has only one text child, means it has no errors, since one of the childs is always the label text',
     );
 
     final button = tester.widget<RaisedButton>(find.byType(RaisedButton));
-    expect(button.onPressed, null);
+    expect(
+      button.onPressed,
+      null,
+    );
   });
 }
