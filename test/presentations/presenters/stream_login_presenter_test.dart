61f7129b219166bc32032fe0e065a1dfa5f05120
DavidCarrilho <luan.david.oliver@gmail.com>
2022-11-23 22:01:49 -0300

    refactor: move StreamLoginPresenter to its own file

--- test/presentations/presenters/stream_login_presenter_test.dart
+++ test/presentations/presenters/stream_login_presenter_test.dart
@@ -1,37 +1,8 @@
-import 'dart:async';
-
 import 'package:faker/faker.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
-
-abstract class Validation {
-  String validate({
-    @required String field,
-    @required String value,
-  });
-}
-
-class LoginState {
-  String emailError;
-}
-
-class StreamLoginPresenter {
-  final Validation validation;
-  final _controller = StreamController<LoginState>.broadcast();
-
-  var _state = LoginState();
-
-  Stream<String> get emailErrorStream => _controller.stream.map((state) => state.emailError);
-
-  StreamLoginPresenter({
-    @required this.validation,
-  });
-  void validateEmail(String email) {
-    _state.emailError = validation.validate(field: 'email', value: email);
-    _controller.add(_state);
-  }
-}
+import 'package:prodev/presentations/dependences/dependences.dart';
+import 'package:prodev/presentations/presenters/presenters.dart';
 
 class ValidationSpy extends Mock implements Validation {}
 
