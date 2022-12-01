07ada3fc762c8faae5c4bb2e9ef0b4b8ff3c6d4d
DavidCarrilho <luan.david.oliver@gmail.com>
2022-11-30 21:21:42 -0300

    teste: add ValidatorBilder

--- lib/main/builders/validation_builder.dart
+++ lib/main/builders/validation_builder.dart
@@ -3,22 +3,24 @@ import 'package:prodev/validation/validators/validators.dart';
 
 class ValidationBuilder {
   static ValidationBuilder _instance;
-  String filedName;
+  String fieldName;
   List<FieldValidation> validations = [];
 
+  ValidationBuilder._();
+
   static ValidationBuilder field(String fieldName) {
-    _instance = ValidationBuilder();
-    _instance.filedName = fieldName;
+    _instance = ValidationBuilder._();
+    _instance.fieldName = fieldName;
     return _instance;
   }
 
   ValidationBuilder required() {
-    validations.add(RequiredFieldValidation(filedName));
+    validations.add(RequiredFieldValidation(fieldName));
     return this;
   }
   
   ValidationBuilder email() {
-    validations.add(RequiredFieldValidation(filedName));
+    validations.add(EmailValidation(fieldName));
     return this;
   }
 
--- lib/main/factories/pages/login/login_validation_factory.dart
+++ lib/main/factories/pages/login/login_validation_factory.dart
@@ -1,3 +1,4 @@
+import 'package:prodev/main/builders/validation_builder.dart';
 import 'package:prodev/presentation/dependences/dependences.dart';
 import 'package:prodev/validation/dependences/dependences.dart';
 import 'package:prodev/validation/validators/validators.dart';
@@ -8,8 +9,7 @@ Validation makeLoginValidation() {
 
 List<FieldValidation> makeLoginValidations() {
   return [
-    RequiredFieldValidation('email'),
-    EmailValidation('email'),
-    RequiredFieldValidation('password'),
+    ...ValidationBuilder.field('email').required().email().build(),
+    ...ValidationBuilder.field('password').required().build(),
   ];
 }
